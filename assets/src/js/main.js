14d941e112130542a00d87198422e611ac001458
abradifcode <bookdw@gmail.com>
2019-03-23 17:32:32 +0300

    my sourse files for Btstr3-gulp

---
+++ assets/src/js/main.js
@@ -0,0 +1,22 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 3
+//= ../../../node_modules/bootstrap/js/dist/affix.js
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/transition.js
+
+// Импортируем другие js-файлы
+//= my.js
